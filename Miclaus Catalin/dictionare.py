cc4a56c0b171ceecb37c23bd04a1fd76fb206a7e
miclauscatalin <miclaus.catalin@gmail.com>
2024-12-05 13:45:17 +0200

    Adăugat conținut nou

---
+++ Miclaus Catalin/dictionare.py
@@ -0,0 +1,7 @@
+def dictionar();
+    student={"Nume":"Maria","Prenume":"Ioana","varsta":21}
+    for cheie,valoare in student.items ():
+        print(cheie,valoare)
+        
+def lista_dictionare():""
+def cautare_in_dictiona():""
