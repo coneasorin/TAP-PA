98ff2943ef4844c0174bbb32a47ed50287cd1f53
Flavius23PA <flavius-marian.igescu23@ing.ub.ro>
2024-12-11 21:47:26 +0200

    sterg toate fisierele de pe branch

---
+++ IGESCU_PA/PythonApplication1_SortareSecventiala.py
@@ -0,0 +1,13 @@
+﻿n = int(input("Introdu numărul de elemente: "))
+v = []
+
+for i in range(n):
+    element = int(input(f"Introdu V[{i}]= "))
+    v.append(element)
+
+element_cautat = int(input("Introdu elementul căutat: "))
+
+for i in range(n):
+    if v[i] == element_cautat:
+        print(f"Elementul căutat se află la poziția: {i}")
+          
