782a6cb6cf639f0dd8ca1767a36b6fa79c122124
Flavius23PA <flavius-marian.igescu23@ing.ub.ro>
2024-12-11 22:11:10 +0200

    am incarcat din nou fisierele

---
+++ IGESCU_PA/COD_SIMPLU_PYTHON_21.11.2024.py
@@ -0,0 +1,10 @@
+﻿def verificare_nume():
+    a = 5
+    b = 10
+    if a > b:
+        print("DA")
+    else:
+        print("NU")
+
+# Apelăm funcția pentru a verifica rezultatul
+verificare_nume()
