add7f5d98e7b23454de5ae40c618d053d8ea084a
miclauscatalin <127289719+miclauscatalin@users.noreply.github.com>
2024-12-12 11:45:24 +0200

    Update jocfoarfeca1.py
    aranjat in pagina pentru redability

--- Miclaus Catalin/jocfoarfeca1.py
+++ Miclaus Catalin/jocfoarfeca1.py
@@ -2,43 +2,43 @@ from tkinter import *
 from tkinter import messagebox
 import random
 
-# Function to determine the winner
+# Determinam castigatorul
 def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         return "Draw"
     elif (user_choice == "Piatra" and computer_choice == "Hartie") or \
          (user_choice == "Hartie" and computer_choice == "Foarfeca") or \
          (user_choice == "Foarfeca" and computer_choice == "Piatra"):
-        return "Computer Wins"
+        return "COMPUTERUL A CASTIGAT"
     else:
-        return "You Win"
+        return "AI CASTIGAT"
 
-# Function to handle user button click
+# Function atasata la user click
 def user_choice(choice):
-    # Randomly generate the computer's choice
+    # genereaza random alegerea computerului
     computer_choice = random.choice(["Piatra", "Hartie", "Foarfeca"])
     
-    # Determine the result
+    # Determinam rezultatul
     result = determine_winner(choice, computer_choice)
     
-    # Display the result in a message box
+    # Afisam rezultatul in messagebox
     messagebox.showinfo("Rezultatul Jocului", f"Tu ai ales {choice}\nCalculatorul a ales {computer_choice}\n\n{result}")
 
-# Create the main window
+# Creaza main window
 afisaj = Tk()
 afisaj.title("Piatra, Hartie, Foarfeca")
 
-# Define the game rules
+# Defineste regulile jocului
 reguli = """Regulile jocului sunt:
 Piatra vs Hartie -> Hartie
 Piatra vs Foarfeca -> Piatra
 Hartie vs Foarfeca -> Foarfeca"""
 
-# Create a label to display the rules
+# Creeaza o eticheta sa afisezi regulile jocului
 label_reguli_joc = Label(afisaj, text=reguli, fg="red")
 label_reguli_joc.pack(side="top", padx=10, pady=10)
 
-# Create buttons for the user to choose between Rock, Paper, or Scissors
+# Butoane pentru Piatra Hartie Foarfeca
 button_piatra = Button(afisaj, text="Piatra", width=20, command=lambda: user_choice("Piatra"))
 button_piatra.pack(side="left", padx=10)
 
@@ -48,5 +48,5 @@ button_hartie.pack(side="left", padx=10)
 button_foarfeca = Button(afisaj, text="Foarfeca", width=20, command=lambda: user_choice("Foarfeca"))
 button_foarfeca.pack(side="left", padx=10)
 
-# Run the main loop
+# ruleaza main
 afisaj.mainloop()
