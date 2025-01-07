e52d7c0424df8d7c65a7bf5e2230ffe0789bd4de
eemiliano55 <emidenisyanis55@gmail.com>
2025-01-07 20:51:07 +0200

    Delete Miclaus Catalin directory

--- Miclaus Catalin/dictionare.py
+++
@@ -1,7 +0,0 @@
-def dictionar();
-    student={"Nume":"Maria","Prenume":"Ioana","varsta":21}
-    for cheie,valoare in student.items ():
-        print(cheie,valoare)
-        
-def lista_dictionare():""
-def cautare_in_dictiona():""
